afa1d6c75c44adfd0b330d985f9a5945a0d73229
softprops <d.tangren@gmail.com>
2014-12-29 23:54:33 -0500

    some specs for backwards clocks that turn the wrong way

--- src/test/scala/GeneratorSpec.scala
+++ src/test/scala/GeneratorSpec.scala
@@ -8,5 +8,24 @@ class GeneratorSpec extends FunSpec {
       assert(Generator(workerId = -1).isLeft)
       assert(Generator(datacenterId = -1).isLeft)
     }
+
+    it ("should recognize backwards winding clocks") {
+      val clock = 
+      Generator(prev = Generator.State(Clock.default() + 1000, 0))
+       .fold(fail(_), { gen =>
+          assert(gen.next().isLeft)
+       })
+    }
+    
+    it ("should seq at ids that are requested at the same time") {
+      val frozen = new Clock {
+        val apply = System.currentTimeMillis
+      }
+      Generator(clock = frozen)
+       .fold(fail(_), { gen =>
+         println(gen.next())
+         println(gen.next())
+       })
+    }
   }
 }
